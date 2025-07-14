9136ae61e7c067cbcc24d0f9e3827204b12f876b
Mohamedxzxzmm <momexprs888@gmail.com>
2025-07-14 10:13:03 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -19,6 +17,7 @@ export default function Home() {
             المتصفح لا يدعم تشغيل الفيديو.
           </video>
         </div>
-        
+      </main>
+    </div>
   );
 }
