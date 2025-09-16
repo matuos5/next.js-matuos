32eb91a200ddd03a50b93fde3bbd03a8c966716f
Mohamedxzxzmm <momexprs888@gmail.com>
2025-09-16 22:08:06 +0300

    Update route.js

--- app/api/snaptik/route.js
+++ app/api/snaptik/route.js
@@ -1,45 +1,28 @@
-import { NextResponse } from "next/server";
-import { chromium } from "playwright-chromium";
+import axios from 'axios';
 
-export async function GET(req) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const tiktokUrl = searchParams.get("url");
-
-    if (!tiktokUrl) {
-      return NextResponse.json({ code: 0, msg: "No URL provided", data: null }, { status: 400 });
+const response = await axios.post(
+  'https://ttsave.app/download',
+  {
+    'query': 'https://vt.tiktok.com/ZSDB8qqvY/',
+    'language_id': '1'
+  },
+  {
+    headers: {
+      'Host': 'ttsave.app',
+      'Connection': 'keep-alive',
+      'Content-Length': '62',
+      'sec-ch-ua-platform': '"Android"',
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2007J20CG Build/SKQ1.211019.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.7258.160 Mobile Safari/537.36',
+      'sec-ch-ua': '"Not;A=Brand";v="99", "Android WebView";v="139", "Chromium";v="139"',
+      'sec-ch-ua-mobile': '?1',
+      'Origin': 'https://ttsave.app',
+      'X-Requested-With': 'mark.via.gp',
+      'Sec-Fetch-Site': 'same-origin',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Dest': 'empty',
+      'Referer': 'https://ttsave.app/en',
+      'Accept-Encoding': 'gzip, deflate, br, zstd',
+      'Accept-Language': 'ar,en-GB;q=0.9,en-US;q=0.8,en;q=0.7'
     }
-
-    // تشغيل Chromium باستخدام Playwright
-    const browser = await chromium.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(tiktokUrl, { waitUntil: "networkidle" });
-
-    // جلب رابط الفيديو من صفحة تيك توك
-    const videoLink = await page.evaluate(() => {
-      const video = document.querySelector("video");
-      return video ? video.src : null;
-    });
-
-    await browser.close();
-
-    if (!videoLink) {
-      return NextResponse.json({ code: 0, msg: "No video found", data: null });
-    }
-
-    return NextResponse.json({
-      code: 1,
-      msg: "success",
-      data: { link: videoLink },
-    });
-
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({
-      code: 0,
-      msg: "Error fetching video",
-      error: error.message,
-      data: null,
-    }, { status: 500 });
   }
-}
+);
